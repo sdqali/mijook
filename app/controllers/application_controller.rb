9bcdfa9fe5f25ec46184569acd7aa352d96f2dc1
Sadique Ali <sadiqalikm@gmail.com>
2011-11-19 13:43:06 +0530

    Got basic stuff working. You can queue songs for playing by dropping in URLs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+require 'vlc_client'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  VLC = VlcClient.new "localhost", "8765"
 end
--- lib/vlc_client.rb
+++ lib/vlc_client.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'cgi'
 require 'httpclient'
+require 'rexml/document'
 
 class VlcClient
   def initialize host, port
@@ -15,10 +16,11 @@ class VlcClient
     post(enqueue_url)
   end
 
-  def play video_url=nil
-    play_url =  command(:command => "in_play",
-                        :input => url_for(video_url))
-    post(play_url)
+  def play
+    resp = get(status_url)
+    if get_state(resp) != "playing"
+      post(command(:command => "in_play"))
+    end
   end
 
   def stop
@@ -53,5 +55,18 @@ class VlcClient
 
   def post url
     @client.post url
+  end
+
+  def status_url
+    "#{base_url}/requests/status.xml"
+  end
+
+  def get url
+    @client.get(url).body
+  end
+
+  def get_state resp
+    doc = REXML::Document.new(resp)
+    REXML::XPath.first(doc, "//state").text
   end
 end
