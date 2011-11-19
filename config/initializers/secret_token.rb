2889a3dc1b9908371b33f77dcb99de3c5ed4af12
Sadique Ali <sadiqalikm@gmail.com>
2011-11-19 12:12:52 +0530

    Initial commit. Empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mijook::Application.config.secret_token = '9d12ebdfb6d163bd6456803dbf486ad9a01d63a949e211b577d0ea47d1a95471eee26df20baac663694867e77d954e0704f15ea047abc036aaaf13e2c72dcf48'
